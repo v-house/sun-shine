074ca61857598f8d019793edea1d9825d71f3d7b
Vivek <365anu65@gmail.com>
2023-10-27 22:51:55 +0530

    Simple about app page...

--- app/aboutapp/Testing.tsx
+++ app/aboutapp/Testing.tsx
@@ -1,45 +1,37 @@
 import React from "react";
+import { Tabs, Tab, Card, CardBody, CardHeader } from "@nextui-org/react";
 
-const NftBanner: React.FC = () => {
+export default function App() {
+  let tabs = [
+    {
+      id: "Access",
+      label: "Quick Access",
+      content:
+        "Get quick access to all important sunshine member details including counsellors, faculty members and student group.",
+    },
+    {
+      id: "Slot",
+      label: "Book Slot",
+      content:
+        "Book a slot for a meet with counsellors. Let the counsellors help you even more effectively.",
+    },
+  ];
+
   return (
-    <div className="flex flex-col justify-center items-center h-[100vh]">
-      <div
-        className="flex flex-col rounded-md bg-cover px-[30px] py-[30px] md:px-[64px] md:py-[56px] max-w-[95%] xl:max-w-[800px] w-[95%]"
-        style={{
-          backgroundImage:
-            'url("https://raw.githubusercontent.com/horizon-ui/horizon-tailwind-react-ts-corporate/main/src/assets/img/nfts/NftBanner1.png")',
-        }}
-      >
-        <div className="w-full">
-          <h4 className="mb-[14px] max-w-full text-xl font-bold text-white md:w-[64%] md:text-3xl md:leading-[42px] lg:w-[46%] xl:w-[85%] 2xl:w-[75%] 3xl:w-[52%]">
-            Discover, collect, and sell extraordinary NFTs
-          </h4>
-          <p className="mb-[40px] max-w-full text-base font-medium text-[#E3DAFF] md:w-[64%] lg:w-[40%] xl:w-[72%] 2xl:w-[60%] 3xl:w-[45%]">
-            Enter in this creative world. Discover now the latest NFTs or start
-            creating your own!
-          </p>
-          <div className="mt-[36px] flex items-center justify-between gap-4 sm:justify-start 2xl:gap-10">
-            <button className="text-black linear rounded-md bg-white px-4 py-2 text-center text-base font-medium transition duration-200 hover:!bg-white/80 active:!bg-white/70">
-              Discover now
-            </button>
-            <button className="text-base font-medium text-lightPrimary hover:text-lightPrimary 2xl:ml-2">
-              Watch Video
-            </button>
-          </div>
-        </div>
-      </div>
-      <p className="font-normal text-navy-700 mt-20 mx-auto w-max">
-        Profile Card component from{" "}
-        <a
-          href="https://horizon-ui.com?ref=tailwindcomponents.com"
-          target="_blank"
-          className="text-brand-500 font-bold"
-        >
-          Horizon UI Tailwind React
-        </a>
-      </p>
+    <div className="flex w-full flex-col mt-4">
+      <Tabs aria-label="Dynamic tabs" items={tabs} className="">
+        {(item) => (
+          <Tab
+            key={item.id}
+            title={item.label}
+            className="bg-transparent pl-0 p-4"
+          >
+            <Card className="h-44 bg-yellow-400 rounded-lg shadow-none">
+              <CardBody className="text-black">{item.content}</CardBody>
+            </Card>
+          </Tab>
+        )}
+      </Tabs>
     </div>
   );
-};
-
-export default NftBanner;
+}
--- app/aboutapp/page.tsx
+++ app/aboutapp/page.tsx
@@ -1,5 +1,17 @@
+"use client";
 import React from "react";
-import NftBanner from "./Testing";
+
+import { Accordion, AccordionItem } from "@nextui-org/react";
+import {
+  FaUser,
+  FaUserMd,
+  FaChalkboardTeacher,
+  FaUsers,
+  FaBusinessTime,
+  FaUsersCog,
+  FaUserFriends,
+} from "react-icons/fa";
+import App from "./Testing";
 
 const BestDealSection: React.FC = () => {
   return (
@@ -8,17 +20,17 @@ const BestDealSection: React.FC = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 items-center max-w-6xl mx-auto">
           <div className="px-4 md:mr-6 mt-2 animate-jump-in animate-ease-in-out">
             <h3 className="text-4xl pr-6 sm:leading-snug tracking-tight font-bold text-black">
-              With our diverse range of lenders, we fight hard to get you the
-              best deal.
+              Sunshine is happily announcing its first App Version for the
+              students welfare.
             </h3>
             <p className="mt-4 text-stone-800 text-xl font-medium">
-              With our diverse range of lenders, we fight hard to get you the
-              best deal.
+              To help you reach out to us even more easily and quickly.
             </p>
+            <App />
           </div>
           <div className="animate-fade-left animate-ease-in-out">
             <div className="bg-yellow-400 transform -translate-x-10 relative h-64 rounded-lg"></div>
-            <div className="transform md:rounded-md bg-white rotate-3 scale-110 translate-x-10 shadow-lg -ml-4 -mt-44 p-8 space-y-2">
+            <div className="transform md:rounded-md bg-white scale-110 translate-x-10 shadow-lg -ml-4 -mt-44 p-8 space-y-2">
               <div className="px-4 sm:px-0 grid grid-cols-5 sm:grid-cols-4 gap-2 sm:gap-8 max-w-5xl mx-auto">
                 <img src="./app1.png" alt="Lender 1" />
                 <img src="./app3.png" alt="Lender 1" />
@@ -29,139 +41,6 @@ const BestDealSection: React.FC = () => {
           </div>
         </div>
       </section>
-      <section className="bg-gradient-to-b from-yellow-300 to-white rounded-l-lg">
-        <div className="container mx-auto flex px-5 py-12 md:flex-row flex-col items-center">
-          <div
-            className="lg:max-w-lg lg:w-full md:w-1/2 w-3/4 mx-auto"
-            data-taos-offset="400"
-          >
-            <img
-              className="object-cover mx-auto h-screen border border-zinc-900"
-              alt="hero"
-              src="./app1.png"
-            />
-          </div>
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center p-6">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-900">
-              Welcome to the Sunshine App
-            </h1>
-            <p className="mb-8 leading-relaxed text-gray-700">
-              Discover the extraordinary in every day. Sunshine is your portal
-              to a world of inspiration and happiness. Explore and experience
-              life in a whole new way.
-            </p>
-            <a
-              href="/aboutapp"
-              className="relative inline-flex items-center justify-start px-6 py-3 mt-2 overflow-hidden font-medium transition-all bg-sky-300 text-black rounded hover:bg-blue-600 group shadow-md"
-            >
-              <span className="w-48 h-48 rounded rotate-[-40deg] bg-blue-700 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-              <span className="relative w-full text-left transition-colors duration-300 ease-in-out group-hover:text-white">
-                View App Features
-              </span>
-            </a>
-          </div>
-        </div>
-      </section>
-      <section className="bg-gradient-to-b from-white to-yellow-300 rounded-l-lg">
-        <div className="container mx-auto flex px-5 py-12 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center p-6">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-900">
-              Welcome to the Sunshine App
-            </h1>
-            <p className="mb-8 leading-relaxed text-gray-700">
-              Discover the extraordinary in every day. Sunshine is your portal
-              to a world of inspiration and happiness. Explore and experience
-              life in a whole new way.
-            </p>
-            <a
-              href="/aboutapp"
-              className="relative inline-flex items-center justify-start px-6 py-3 mt-2 overflow-hidden font-medium transition-all bg-sky-300 text-black rounded hover:bg-blue-600 group shadow-md"
-            >
-              <span className="w-48 h-48 rounded rotate-[-40deg] bg-blue-700 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-              <span className="relative w-full text-left transition-colors duration-300 ease-in-out group-hover:text-white">
-                View App Features
-              </span>
-            </a>
-          </div>
-          <div
-            className="lg:max-w-lg lg:w-full md:w-1/2 w-3/4 mx-auto"
-            data-taos-offset="400"
-          >
-            <img
-              className="object-cover mx-auto h-screen border border-zinc-900"
-              alt="hero"
-              src="./app3.png"
-            />
-          </div>
-        </div>
-      </section>
-      <section className="bg-gradient-to-b from-yellow-300 to-white rounded-l-lg">
-        <div className="container mx-auto flex px-5 py-12 md:flex-row flex-col items-center">
-          <div
-            className="lg:max-w-lg lg:w-full md:w-1/2 w-3/4 mx-auto"
-            data-taos-offset="400"
-          >
-            <img
-              className="object-cover mx-auto h-screen border border-zinc-900"
-              alt="hero"
-              src="./app4.png"
-            />
-          </div>
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center p-6">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-900">
-              Welcome to the Sunshine App
-            </h1>
-            <p className="mb-8 leading-relaxed text-gray-700">
-              Discover the extraordinary in every day. Sunshine is your portal
-              to a world of inspiration and happiness. Explore and experience
-              life in a whole new way.
-            </p>
-            <a
-              href="/aboutapp"
-              className="relative inline-flex items-center justify-start px-6 py-3 mt-2 overflow-hidden font-medium transition-all bg-sky-300 text-black rounded hover:bg-blue-600 group shadow-md"
-            >
-              <span className="w-48 h-48 rounded rotate-[-40deg] bg-blue-700 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-              <span className="relative w-full text-left transition-colors duration-300 ease-in-out group-hover:text-white">
-                View App Features
-              </span>
-            </a>
-          </div>
-        </div>
-      </section>
-      <section className="bg-gradient-to-b from-white to-yellow-300 rounded-l-lg">
-        <div className="container mx-auto flex px-5 py-12 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center p-6">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-900">
-              Welcome to the Sunshine App
-            </h1>
-            <p className="mb-8 leading-relaxed text-gray-700">
-              Discover the extraordinary in every day. Sunshine is your portal
-              to a world of inspiration and happiness. Explore and experience
-              life in a whole new way.
-            </p>
-            <a
-              href="/aboutapp"
-              className="relative inline-flex items-center justify-start px-6 py-3 mt-2 overflow-hidden font-medium transition-all bg-sky-300 text-black rounded hover:bg-blue-600 group shadow-md"
-            >
-              <span className="w-48 h-48 rounded rotate-[-40deg] bg-blue-700 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-              <span className="relative w-full text-left transition-colors duration-300 ease-in-out group-hover:text-white">
-                View App Features
-              </span>
-            </a>
-          </div>
-          <div
-            className="lg:max-w-lg lg:w-full md:w-1/2 w-3/4 mx-auto"
-            data-taos-offset="400"
-          >
-            <img
-              className="object-cover mx-auto h-screen border border-zinc-900"
-              alt="hero"
-              src="./app5.png"
-            />
-          </div>
-        </div>
-      </section>
-      <NftBanner />
     </>
   );
 };
