6223daffb66eceab2de0fa714b16e0ce6cde6f57
Vivek <365anu65@gmail.com>
2023-10-27 16:21:36 +0530

    Improvised team panel on homepage...

--- app/Team.tsx
+++ app/Team.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef } from "react";
+import { useRouter } from "next/navigation";
 
 const MyComponent = () => {
+  const router = useRouter();
   const sidebarRef = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
@@ -12,6 +14,19 @@ const MyComponent = () => {
     }
   }, []);
 
+  const handleFacultyClick = () => {
+    router.push("/teams");
+  };
+  const handleManagementClick = () => {
+    router.push("/teams");
+  };
+  const handleBuddiesClick = () => {
+    router.push("/buddies");
+  };
+  const handleMentorsClick = () => {
+    router.push("/mentors");
+  };
+
   return (
     <div className="relative p-2 lg:p-4 bg-sky-300">
       <div className="relative overflow-hidden rounded-lg">
@@ -20,28 +35,40 @@ const MyComponent = () => {
           alt="Team 2023"
           className="w-full h-auto transition-transform duration-500 transform hover:scale-105"
         />
+        <div className="absolute inset-0 bg-gradient-to-l from-gray-900 via-gray-900/40"></div>
         <aside
           ref={sidebarRef}
-          className="absolute top-0 right-0 w-1/4 lg:w-1/5 h-full flex flex-col justify-center text-xl lg:text-4xl font-bold opacity-0 transition-transform duration-500"
+          className="absolute top-0 right-0 w-1/4 lg:w-1/5 h-full pr-12 flex flex-col justify-center items-end text-xl lg:text-4xl font-bold opacity-0 transition-transform duration-500"
           style={{
-            background: "rgba(0, 0, 0, 0.6)",
             transform: "translateY(-100%)",
           }}
         >
-          <div className="text-white text-xl lg:text-4xl font-bold mb-10 text-center">
+          <div className="text-white text-xl lg:text-4xl font-extrabold mb-6 md:mb-10 lg:mb-16">
             TEAM
           </div>
-          <div className="flex flex-col space-y-2">
-            <button className="text-white bg-opacity-60 hover:scale-105 px-2 py-1 text-xs lg:text-sm">
+          <div className="flex flex-col space-y-1 md:space-y-2 lg:space-y-4 items-end">
+            <button
+              onClick={handleFacultyClick}
+              className="text-white bg-opacity-60 hover:scale-105 py-1 text-xs lg:text-xl transition-transform duration-400 hover:-translate-x-2"
+            >
               Faculty
             </button>
-            <button className="text-white bg-opacity-60 hover:scale-105 px-2 py-1 text-xs lg:text-sm">
+            <button
+              onClick={handleManagementClick}
+              className="text-white bg-opacity-60 hover:scale-105 py-1 text-xs lg:text-xl transition-transform duration-400 hover:-translate-x-2"
+            >
               Management
             </button>
-            <button className="text-white bg-opacity-60 hover:scale-105 px-2 py-1 text-xs lg:text-sm">
+            <button
+              onClick={handleBuddiesClick}
+              className="text-white bg-opacity-60 hover:scale-105 py-1 text-xs lg:text-xl transition-transform duration-400 hover:-translate-x-2"
+            >
               Buddies
             </button>
-            <button className="text-white bg-opacity-60 hover:scale-105 px-2 py-1 text-xs lg:text-sm">
+            <button
+              onClick={handleMentorsClick}
+              className="text-white bg-opacity-60 hover:scale-105 py-1 text-xs lg:text-xl transition-transform duration-400 hover:-translate-x-2"
+            >
               Mentors
             </button>
           </div>
