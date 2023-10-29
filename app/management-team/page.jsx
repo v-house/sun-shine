b36d32bec0bd4063c48c7f8451a37a708caa58fa
adv8j <advait.jain2003@gmail.com>
2023-10-29 19:48:28 +0530

    added responsiveness to management page

--- app/management-team/page.jsx
+++ app/management-team/page.jsx
@@ -3,20 +3,24 @@ import React, { useState } from "react";
 import css from "./App.css";
 import teamMembers from "app/management.js";
 import TeamCard from "./TeamCard.tsx";
+import { Button, ButtonGroup } from "@nextui-org/react";
 export default function App() {
   const [currentMember, setcurrentMember] = useState("Finance Team Member");
-  function setDisplay(current) {
-    setcurrentMember(current.target.name);
-    // console.log(currentMember);
-  }
   function NavItem(props) {
     var teamName;
     if (props.name.includes("Member")) {
       teamName = props.name.replace("Member", "");
     } else teamName = props.name;
     function setDisplay() {
+      var sideNav = document.querySelector(".show-nav");
+      if (sideNav != null) {
+        sideNav.classList.toggle("show-nav");
+        document
+        .querySelector(".show-nav-details")
+        .classList.toggle("show-nav-details");
+      }
       setcurrentMember(props.name);
-      console.log(props.name);
+      // console.log(props.name);
     }
     return (
       <>
@@ -34,15 +38,22 @@ export default function App() {
     } else teamName = current;
     return teamName;
   }
+  function showSideNav() {
+    var sideNav = document.querySelector(".side-nav");
+    sideNav.classList.add("show-nav");
+    document.querySelector(".details").classList.add("show-nav-details");
+  }
   return (
     <div className="main-container">
-    <div className="side-nav">
+      <div className="side-nav">
         {Object.entries(teamMembers).map(([key, value]) => (
-          <NavItem
-            key={key}
-            name={key}
-          />
+          <NavItem key={key} name={key} />
         ))}
+      </div>
+      <div className="side-button">
+        <Button color="primary" onPress={showSideNav} className="mb-3">
+          View Teams
+        </Button>
       </div>
       <div className="details">
         <h1 className="text-3xl mb-1">{teamName(currentMember)}</h1>
@@ -53,7 +64,6 @@ export default function App() {
         </p>
         <h2 className="text-2xl font-semibold mt-1 mb-1">Members</h2>
         <div className="member-cards">
-          {console.log(currentMember)}
           {teamMembers[currentMember].map((member, index) => (
             <TeamCard key={index} {...member} />
           ))}
