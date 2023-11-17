3d9242baf41a72e4e1e8e9e0493b2479ff66c01c
Vivek <365anu65@gmail.com>
2023-11-17 16:47:07 +0530

    Events page size....

--- app/events/infiniteScroll.tsx
+++ app/events/infiniteScroll.tsx
@@ -1,29 +1,37 @@
 "use client";
 import events from "./events";
 
-function InfiniteScroll(){
-    return(
-        <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
-            <ul className=" animate-infinite-scroll flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none">
-            {events.map((entry, index) => {
-                return (
-                    <li key={entry.slug}>
-                    <img src={entry.images[1]} alt={entry.title} className='rounded-lg h-80 w-96 md:max-h-80 md:max-w-96 object-cover border-4 border-sky-100 shadow-md'/> 
-                    </li>  
-                );
-            })}
-            </ul>  
-            <ul className=" animate-infinite-scroll flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none">
-                {events.map((entry, index) => {
-                    return (
-                        <li key={entry.slug}>
-                        <img src={entry.images[1]} alt={entry.title} className='rounded-lg h-80 w-96 md:max-h-80 md:max-w-96  object-cover border-4 border-sky-100 shadow-md'/> 
-                        </li>  
-                    );
-                })}
-            </ul>              
-        </div>
-    )
+function InfiniteScroll() {
+  return (
+    <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_56px,_black_calc(100%-56px),transparent_100%)]">
+      <ul className=" animate-infinite-scroll flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none">
+        {events.map((entry, index) => {
+          return (
+            <li key={entry.slug}>
+              <img
+                src={entry.images[1]}
+                alt={entry.title}
+                className="rounded-lg aspect-video md:max-h-80 md:max-w-96 object-cover border-4 border-sky-100 shadow-md"
+              />
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="animate-infinite-scroll flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none">
+        {events.map((entry, index) => {
+          return (
+            <li key={entry.slug}>
+              <img
+                src={entry.images[1]}
+                alt={entry.title}
+                className="rounded-lg h-80 w-96 md:max-h-80 md:max-w-96  object-cover border-4 border-sky-100 shadow-md"
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default InfiniteScroll;
