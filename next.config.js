33868bbb4f610d1b06fbae68d3145bfc7daeaef4
Vivek <365anu65@gmail.com>
2024-02-01 14:42:01 +0530

    distribution made, github workflow initialized..

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
+  output: "export",
+  distDir: "dist",
 };
 
 module.exports = nextConfig;
